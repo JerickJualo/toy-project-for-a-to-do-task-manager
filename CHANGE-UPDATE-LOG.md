38c20d7814e65a0d257c76d91f7272b7e7251245
JerickJualo <jualojerick1@gmail.com>
2025-05-31 16:27:37 +0800

    Add the basic structure of my project in main_module.py

--- CHANGE-UPDATE-LOG.md
+++ CHANGE-UPDATE-LOG.md
@@ -1,1 +1,19 @@
+### Progress Log
 
+---
+
+### 2025-05-31
+
+This project started as a To-Do Task Manager script and will grow into a collection of small utilities. For now, it includes the topics i learned:
+
+- Python Syntax
+- Print Statement
+- Comments
+- Variables
+
+Files:
+
+- `src/main_module.py` – create the basic structor for my To-Do Task Manager”
+- Add Chage-Update-Log to seprate the Log for my Updates from README.md
+
+--- 
--- src/main_module.py
+++ src/main_module.py
@@ -1,8 +1,60 @@
-def main():
-    print("Hello, world!")
+print("My Simple To-Do Task Manager")
+print("==========================")
+print("Heres my Everday Tasks for this Summer Vacation")
+print("==========================")
+print("Goals:")
+print("1. Solidify my Python skills from Basic to Data Collections to Functions to File and Error Handling to OOP")
+print("2. Learn PostgreSQL")
+print("3. Study Math/Statistics for Data Science At least Study and Solidify my Math Skills in Algebra")
+print("==========================")
 
-if __name__ == "__main__":
-    main()
+print("Starting June 1, 2025, Here's the Main Task that i will do Everday:")
+print("""
+Sunday - Do the Toy Projects in Python
+       - Study Math(Getting Ready for Algebra Course in Khan Academy)
+       
+Monday - Study/Code in Python or Do Problem Solving in Python Problems provided by ChatGPT or HackerRank, LeetCode, Codewars
+       - Study SQL/PostgreSQL and Do Problem Solving in SQL Problems provided by ChatGPT
+       
+Tuesday - Study/Code in Python or Do Problem Solving in Python Problems provided by ChatGPT or HackerRank, LeetCode, Codewars
+        - Study Math(Getting Ready for Algebra Course in Khan Academy)
 
-    
+Wednesday - Study/Code in Python or Do Problem Solving in Python Problems provided by ChatGPT or HackerRank, LeetCode, Codewars
+          - Study SQL/PostgreSQL and Do Problem Solving in SQL Problems provided by ChatGPT
+      
+Thursday - Study/Code in Python or Do Problem Solving in Python Problems provided by ChatGPT or HackerRank, LeetCode, Codewars
+         - Study Math(Getting Ready for Algebra Course in Khan Academy)
 
+Friday - Study/Code in Python or Do Problem Solving in Python Problems provided by ChatGPT or HackerRank, LeetCode, Codewars
+       - Study SQL/PostgreSQL and Do Problem Solving in SQL Problems provided by ChatGPT
+
+Saturday - Do the Toy Projects in Python 
+         - Study Math (Getting Ready for Algebra Course in Khan Academy)""")
+         
+ # the rotation of SQL and Math every otherday should be fix as 7 days is a Odd Number 
+ # Today, Saturday (5-31-25) it should be Study SQL and in the next Sunday it should be SQL not Math again
+ 
+
+task_learn_python = "Do the Toy Projects in Python in Morning and Afternoon"
+task_project_python = "Study/Code in Python or Do Problem Solving in Python Problems provided by ChatGPT or HackerRank, LeetCode, Codewars"
+task_math = "Study Math (Getting Ready for Algebra Course in Khan Academy)"
+task_sql = "Study SQL/PostgreSQL and Do Problem Solving in SQL Problems provided by ChatGPT"
+
+print("==========================")
+print("\nToday's Task Finder")
+print("==========================")
+print("What Month is Today?")
+print("- May")
+print("What Day is Today?")
+print("- 31\n")
+print("The date is May 31, 2025 is Saturday")
+print("======Today's Tasks ======")
+
+print(f"""
+ Do the Saturday Tasks:
+ 1. {task_project_python}  
+ 2. {task_sql}             
+     """)
+     
+# use the function f-string and variable task_project_python to get its string value
+# use the function f-string and variable task_sql to get its string value
