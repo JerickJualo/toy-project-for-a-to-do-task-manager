3d2f449defd5776f696c81880f8d49027fae5d4d
JerickJualo <jualojerick1@gmail.com>
2025-06-23 16:44:07 +0800

    6-23-25: define the function create_task on task.py that do the making a task

--- src/tasks.py
+++ src/tasks.py
@@ -12,7 +12,47 @@ class Task:
         self.status = status
         self.date_created = datetime.now()
         self.due_date = None
+        self.day_created = None
 
 
 
+# FUNCTION SECTION:
+
+def create_task():
+    print("Creating a new task:" )
+
+    # a dictionary for task status options
+    status = {"1": "Not Started",
+        "2": "In Progress",
+        "3": "Completed",
+        "4": "On Hold"
+    }
+
+    # Asking the task detials
+
+    task_name: str = input("Enter the task name: ")
+    description: str = input("Enter the task description: ")
+    status_choice: str = input("""Enter the task status: 
+                               (1: Not Started 
+                               2: In Progress
+                               3: Completed
+                               4: On Hold)
+                               : """)
     
+    # Validate the status choice and set the task status
+    if status_choice in status:
+        task_status = status[status_choice]
+    else:
+        print("Invalid status choice. Defaulting to 'Not Started'.")
+        task_status = status["1"]
+
+    #Instantiate a new Task object
+
+    new_task = Task(task_name, description, task_status)
+
+    print(f"""Task '{new_task.task_name}' created successfully with status '{new_task.status}'!
+          Description: {new_task.description}
+          Date Created: {new_task.date_created.strftime('%Y-%m-%d %H:%M:%S')}
+          Week Day Created: {new_task.date_created.strftime("%A")}""")
+          
+    new_task.day_created = new_task.date_created.strftime("%A")
